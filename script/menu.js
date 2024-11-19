d4ee7e6c0900eddd4b707ca0ad4d2e3e526940a4
LeoChau7569 <134211836+LeoChau7569@users.noreply.github.com>
2024-11-19 16:13:53 +0700

    Add files via upload

---
+++ script/menu.js
@@ -0,0 +1,17 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+}
+
+function openNavUser() {
+  document.getElementById("sidenavU").style.width = "160px";
+  document.getElementById("sidenavU").style.height = "125px";
+}
+
+function closeNavUser() {
+  document.getElementById("sidenavU").style.width = "0";
+}
+
