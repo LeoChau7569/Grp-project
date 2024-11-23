085c9ba714690274b747c836fe176c45551d94b4
LeoChau7569 <134211836+LeoChau7569@users.noreply.github.com>
2024-11-23 18:10:10 +0700

    fix a few isue, make heading actually work in all pages

--- script/menu.js
+++ script/menu.js
@@ -1,17 +1,18 @@
 function openNav() {
-  document.getElementById("sidenav").style.width = "200px";
+	document.getElementById("sidenav").style.width = "200px";
 }
 
 function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
+	document.getElementById("sidenav").style.width = "0";
 }
 
 function openNavUser() {
-  document.getElementById("sidenavU").style.width = "150px";
-  document.getElementById("sidenavU").style.height = "125px";
+
+	document.getElementById("sidenavU").style.width = "150px";
+	document.getElementById("sidenavU").style.height = "125px";
 }
 
 function closeNavUser() {
-  document.getElementById("sidenavU").style.width = "0";
+	document.getElementById("sidenavU").style.width = "0";
 }
 
