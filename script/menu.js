f6f28c6a3b93205efdcff7a3318e01635984857a
LeoChau7569 <134211836+LeoChau7569@users.noreply.github.com>
2024-11-26 16:33:36 +0700

    update

--- script/menu.js
+++ script/menu.js
@@ -15,4 +15,14 @@ function closeNavUser() {
 	document.getElementById("sidenavU").style.height = "0";
 	document.getElementById("sidenavU").style.width = "0px";
 }
+function openframe() {
+	document.getElementById("danhsachhocphan").style.display="block";
+	document.getElementById("danhsachhocphan").style.width = "100%";
+	document.getElementById("danhsachhocphan").style.height = "100%";
+}
+function closeframe() {
+	document.getElementById("danhsachhocphan").style.display="block";
+	document.getElementById("danhsachhocphan").style.width = "0";
+	document.getElementById("danhsachhocphan").style.height = "0";
+}
 
